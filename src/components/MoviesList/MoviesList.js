e9060b7e1133eb3bd3aa50c9d79d8d5f4af13df0
Maria <murikmarusia@gmail.com>
2020-02-16 23:37:54 +0200

    Movies App

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
-);
+export const MoviesList = (props) => {
+  const { movies } = props;
+
+  return (
+    <div className="movies">
+      {movies.map(movie => <MovieCard key={movie.imdbId} movie={movie} />)}
+    </div>
+  );
+};
